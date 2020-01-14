3a346557694ce2898cb3c53f86f6f633ca08b291
mukesh murugan <hello@mukeshmurugan.com>
2020-01-14 21:56:44 +0530

    added configs

---
+++ configs/config/config.js
@@ -0,0 +1,8 @@
+const _ = require('lodash');
+const env = process.env.NODE_ENV || 'local';
+const envConfig = require('./' + env);
+let defaultConfig = 
+{
+  env: env
+};
+module.exports = _.merge(defaultConfig, envConfig);
---
+++ configs/database.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    'secret': 'restapisecret',
+    'database': 'mongodb://127.0.0.1:27017/mern-api'
+};
