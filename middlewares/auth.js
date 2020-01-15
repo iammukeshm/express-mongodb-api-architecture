2a16131120b94d32d1cbb2a70445c975c15dada0
mukesh murugan <hello@mukeshmurugan.com>
2020-01-15 23:52:25 +0530

    fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,8 +3,7 @@ const User = require('../models/user');
 const db = require('../configs/database');
 
 const auth = async(req, res, next) => 
-{
-     
+{     
     try 
     {
         const token = req.header('Authorization').replace('Bearer ', ''); 
